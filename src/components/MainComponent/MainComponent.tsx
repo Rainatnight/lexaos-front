fab6cf1a9dfb25865d67b8e43e12b05741a188fb
alex <alexnaf@womanup.online>
2025-09-15 13:30:26 +0300

    feat: favicon,ts,scss

---
+++ src/components/MainComponent/MainComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import cls from "./MainComponent.module.scss";
+
+const MainComponent = () => {
+  return <div className={cls.main}>MainComponent</div>;
+};
+
+export default MainComponent;
