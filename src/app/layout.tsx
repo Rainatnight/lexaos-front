b6ff55f63399298e9502714ee17dc0627d61fc79
alex <alexnaf199@gmail.com>
2025-10-13 17:00:22 +0300

    fet: lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body>
         <Providers>{children}</Providers>
       </body>
