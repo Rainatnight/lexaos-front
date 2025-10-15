df6a81aa875d4de3c4c7d021ae72af971f83a0c5
alex <alexnaf199@gmail.com>
2025-10-15 15:44:34 +0300

    feat: создание акка

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 import cls from "./Login.module.scss";
 import { Input } from "@/shared/api/ui/Input/Input";
 import { useTranslation } from "react-i18next";
-import { Text } from "@/shared/api/ui/Text/Text";
+import { Text, TextVariants } from "@/shared/api/ui/Text/Text";
 import { useRouter } from "next/router";
+import { api } from "@/shared/api/api";
 
 export const Login = () => {
   const router = useRouter();
   const { t } = useTranslation("login");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState("");
+  const [error, setError] = useState<null | string>(null);
 
   const anonLogin = () => {
     router.push("/");
@@ -20,6 +22,18 @@ export const Login = () => {
     console.log(1);
   };
 
+  const createAcc = () => {
+    api
+      .post("/create", { login, password })
+      .then(() => {
+        console.log(1);
+      })
+      .catch((e) => {
+        setError(e.response?.data?.msg);
+      });
+  };
+
+  console.log(error);
   return (
     <div className={cls.wrapper}>
       <div className={cls.background}></div>
@@ -44,12 +58,18 @@ export const Login = () => {
             placeholder={t("Пароль")}
             onChange={(v) => setPassword(v.toLowerCase())}
           />
-          <Text className={cls.enter} onClick={loginFunc}>
-            {t("Войти")}
-          </Text>
-          <Text className={cls.enter} onClick={loginFunc}>
-            {t("Создать аккаунт")}
-          </Text>
+          {login && password && (
+            <>
+              <Text className={cls.enter} onClick={loginFunc}>
+                {t("Войти")}
+              </Text>
+              <Text className={cls.enter} onClick={createAcc}>
+                {t("Создать аккаунт")}
+              </Text>
+            </>
+          )}
+
+          {error && <Text variant={TextVariants.ERROR}>{error}</Text>}
         </div>
 
         <Text className={cls.anon} onClick={anonLogin}>
